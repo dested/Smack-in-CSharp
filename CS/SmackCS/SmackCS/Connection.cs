d7c21c680f56244e4c5025ea9f1613c1edcf2eae
Salvatore Aiello <dested@gmail.com>
2012-04-06 17:31:53 -0700

    Initial Commit. Sharpen not ran yet.

---
+++ CS/SmackCS/SmackCS/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.jivesoftware.smack
+{ 
+    public class Connection
+    {
+        
+
+    }
+}
